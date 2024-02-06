e3cede8e2bc8426c47ebedc630e97b66d689e507
Aidan <130617042+yumyumgummy11@users.noreply.github.com>
2024-02-05 17:17:42 -0700

    Add files via upload

---
+++ grub_randomizer.py
@@ -0,0 +1,61 @@
+from os import listdir, system
+from random import randrange
+
+def get_grub_themes(): #get a list of all themes in the /boot/grub/themes directory
+    theme_list = listdir('/boot/grub/themes')
+    if len(theme_list) == 0:
+        quit()
+    return theme_list
+
+def choose_random_theme(): #choose a random theme based on the list given from the get_grub_themes function
+    theme_list = get_grub_themes()
+    
+    if len(theme_list) > 1:
+        f = ""
+        with open('/etc/default/grub','r') as file:
+            f = file.readlines()
+        
+        remove_queue = []
+        for i in f:
+            if 'GRUB_THEME' in i:
+                for x in theme_list:
+                    if x in i:
+                        remove_queue.append(x)
+                        break
+        
+        for i in remove_queue:
+            theme_list.remove(i)                
+
+    new_theme = theme_list[randrange(0,len(theme_list))]
+    return new_theme
+
+def generate_command(): #generates the command for the grub
+    new_theme = choose_random_theme()
+    theme_command = 'GRUB_THEME="/boot/grub/themes/PLACEHOLDER/theme.txt"'
+    command = theme_command.replace('PLACEHOLDER',new_theme)
+    return command
+
+def make_backup(): #make a backup of the file
+    f = ""
+    with open('/etc/default/grub','r') as file:
+        f = file.read()
+
+    with open('/etc/default/grub_backup.bak','w') as file:
+        file.write(f)
+
+def edit_file(): #edit the grub file and repalce with new command
+    with open('/etc/default/grub','r') as file:
+        data = file.readlines()
+    
+    command = generate_command()
+    for index, i in enumerate(data):
+        if 'GRUB_THEME' in i:
+            data[index] = command + '\n'
+        
+    
+    with open('/etc/default/grub','w') as file:
+        file.writelines(data)
+
+make_backup()
+edit_file()
+system("sudo grub-mkconfig -o /boot/grub/grub.cfg") #run the grub config to apply the changes
